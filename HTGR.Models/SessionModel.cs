535f6a81b65e9db54fe267b46a17e8ddcee87900
Rick van den Bosch <rickvdbosch@outlook.com>
2023-05-03 17:20:33 +0200

    Initial commit in second repo

---
+++ HTGR.Models/SessionModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HTGR.Models
+{
+    public class SessionModel
+    {
+        public string Day { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Duration { get; set; }
+        public string Session { get; set; }
+        public string Description { get; set; }
+        public string Speaker { get; set; }
+        public string Opentoremote { get; set; }
+        public string Teamslink { get; set; }
+        public string Room { get; set; }
+    }
+}
---
+++ HTGR.Web/Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿namespace HTGR.Web.Models
+{
+    public class ProductResult
+    {
+        public ProductModel[] value { get; set; }
+        public string nextLink { get; set; }
+    }
+
+    public class ProductModel
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string ProductNumber { get; set; }
+        public string Color { get; set; }
+        public float StandardCost { get; set; }
+        public float ListPrice { get; set; }
+        public string Size { get; set; }
+        public float? Weight { get; set; }
+        public int ProductCategoryID { get; set; }
+        public int ProductModelID { get; set; }
+        public DateTime SellStartDate { get; set; }
+        public DateTime? SellEndDate { get; set; }
+        public object DiscontinuedDate { get; set; }
+        public string ThumbNailPhoto { get; set; }
+        public string ThumbnailPhotoFileName { get; set; }
+        public string rowguid { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
